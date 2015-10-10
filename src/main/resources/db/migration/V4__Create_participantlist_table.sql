dabd8748a86cba1056a4cc916be7fb30b2c5ff4d
Patrick Nitschke <patrick.nits@googlemail.com>
2015-10-10 10:49:25 +0200

    deez migrations

--- src/main/resources/db/migration/V4__Create_participantlist_table.sql
+++ src/main/resources/db/migration/V4__Create_participantlist_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE participant_list (
+CREATE TABLE participant_lists (
   id         INT          NOT NULL AUTO_INCREMENT,
   user_id    INT          NOT NULL,
   name       VARCHAR(100) NOT NULL,
--- src/main/resources/db/migration/V5__Create_participant_table.sql
+++ src/main/resources/db/migration/V5__Create_participant_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE participant (
+CREATE TABLE participants (
   id                 INT          NOT NULL AUTO_INCREMENT,
   email              VARCHAR(100) NOT NULL UNIQUE,
   PRIMARY KEY (id)
