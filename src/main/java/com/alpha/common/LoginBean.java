af3ac8b4030f108fe4c0ea37ae8bd1e1e1534bae
Erwin Schens <erwin.schens@qurasoft.de>
2015-10-10 15:43:19 +0200

    organized imports in login bean

--- src/main/java/com/alpha/common/LoginBean.java
+++ src/main/java/com/alpha/common/LoginBean.java
@@ -3,14 +3,11 @@ package com.alpha.common;
 import com.alpha.dao.login.LoginDao;
 import com.alpha.models.User;
 import com.alpha.service.UserService;
-import com.alpha.util.Application;
-import com.alpha.util.EmailSender;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import javax.mail.MessagingException;
 import javax.servlet.http.HttpSession;
 import java.io.Serializable;
 
