3cb26ea4f334a53fd32b6fba03ea07193f40c24f
Artur Schens <artur.schens@qurasoft.de>
2015-10-10 12:56:04 +0200

    #37 token random choose, #38 unique token, #39 long token

---
+++ src/main/java/com/alpha/common/TokenBean.java
@@ -0,0 +1,27 @@
+package com.alpha.common;
+
+import com.alpha.models.Token;
+import com.alpha.service.TokenService;
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.util.Date;
+
+/**
+ * Created by arturschens on 10.10.15.
+ */
+@ManagedBean
+@SessionScoped
+public class TokenBean {
+
+    public String getToken() {
+        return "test";
+    }
+
+    public void newToken() {
+        new TokenService().newToken();
+    }
+
+}
