660a6d96b9526f8675e3a683fa0b9aa6d7511f11
Patrick Nitschke <patrick.nits@googlemail.com>
2015-10-10 11:37:49 +0200

    participant listings

---
+++ src/main/resources/db/migration/V7__Create_participant_listings_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE participant_listings (
+  id                  INT NOT NULL AUTO_INCREMENT,
+  participant_id      INT NOT NULL,
+  participant_list_id INT NOT NULL,
+  PRIMARY KEY (id)
+)
