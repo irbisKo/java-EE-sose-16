2c51dd3f498677ce08f5ab7ec8f6644a5ebaedfa
Artur Schens <artur.schens@qurasoft.de>
2015-10-10 18:13:51 +0200

    changed user lazy fetch

--- src/main/java/com/alpha/models/User.java
+++ src/main/java/com/alpha/models/User.java
@@ -39,7 +39,7 @@ public class User implements IModel {
     @Column(name = User.COLUMN_UPDATED_AT, nullable = false)
     private Date updatedAt;
 
-    @OneToMany(mappedBy = ParticipantList.FIELD_USER, fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = ParticipantList.FIELD_USER, fetch = FetchType.LAZY)
     private List<ParticipantList> participantLists;
 
     public Long getId() {
